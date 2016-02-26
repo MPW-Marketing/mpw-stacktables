5e1860d3857274f67d471375e5f3f41ebca7bf09
Damon Maldonado <damon@mpwmarketing.com>
2016-02-26 09:32:24 -0500

    Initial plugin

---
+++ mpw-stacktable.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: MPW Stacktable Enqueue
+Description: Enqueue the Stacktable.js script from http://johnpolacek.github.io/stacktable.js/
+Version: 0.1
+*/
+
+function mpw_stacktable_scripts_method() {
+    wp_enqueue_script( 'stacktable', plugins_url( '/js/stacktable.js' , __FILE__ ), array( 'jquery' ), '1.0', true );
+        wp_enqueue_script( 'mpw-stacktable', plugins_url( '/js/mpw-stacktables.js' , __FILE__ ), array( 'jquery', 'stacktable' ), '1.0', true );
+    }
+
+
+add_action( 'wp_enqueue_scripts', 'mpw_stacktable_scripts_method' );
